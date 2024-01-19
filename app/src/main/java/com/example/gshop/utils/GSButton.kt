732dbfa54a484b82d4b7ec0231a7c0246497d208
ghis8 <ghislainkongolo0@gmail.com>
2024-01-19 19:32:51 +0200

    create reusable xml files

---
+++ app/src/main/java/com/example/gshop/utils/GSButton.kt
@@ -0,0 +1,9 @@
+package com.example.gshop.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class GSButton(context: Context,attrs:AttributeSet):AppCompatButton(context,attrs) {
+
+}
---
+++ app/src/main/java/com/example/gshop/utils/GSEditText.kt
@@ -0,0 +1,9 @@
+package com.example.gshop.utils
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+class GSEditText(context: Context, attrs:AttributeSet):AppCompatEditText(context,attrs) {
+
+}
