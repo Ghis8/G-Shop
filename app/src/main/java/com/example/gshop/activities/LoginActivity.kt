3a7022d489a89656cc30945e0a45b1bbfd061c94
ghis8 <ghislainkongolo0@gmail.com>
2024-01-22 14:09:31 +0200

    added new activities and linking them

--- app/src/main/java/com/example/gshop/activities/LoginActivity.kt
+++ app/src/main/java/com/example/gshop/activities/LoginActivity.kt
@@ -1,12 +1,41 @@
 package com.example.gshop.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import com.example.gshop.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBtn=findViewById<Button>(R.id.btn_login)
+        val email=findViewById<EditText>(R.id.ed_email)
+        val password=findViewById<EditText>(R.id.ed_password)
+        val register=findViewById<TextView>(R.id.tv_register)
+
+        loginBtn.setOnClickListener {
+            if(email.text.toString() == "" || password.text.toString()==""){
+                Toast.makeText(this,"Email and Password Required",Toast.LENGTH_SHORT).show()
+            }else if (email.text.toString()=="ghislain@gmail.com" && password.text.toString()=="password") {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Wrong Credentials",Toast.LENGTH_SHORT)
+            }
+        }
+
+        register.setOnClickListener {
+            val intent=Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
+
 }
---
+++ app/src/main/java/com/example/gshop/activities/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gshop.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import com.example.gshop.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        
+    }
+}
